d81d50ac742171a91b3d16f66c2ae7676bf78770
Keysudarr <ibrahimpyt@outlook.com>
2021-04-11 16:56:18 +0300

    Tinder Card

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,6 @@ class ExampleHomePage extends StatefulWidget {
 class _ExampleHomePageState extends State<ExampleHomePage>
     with TickerProviderStateMixin {
   List<String> welcomeImages = [
-    "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/4.jpg",
-    "images/5.jpg",
     "images/6.jpg",
     "images/7.jpg",
     "images/8.jpg",
